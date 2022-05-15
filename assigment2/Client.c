2cb3f9894a9d45a1c8a203404d6b22e94bf668e3
10EastSea <xgf6580@gmail.com>
2022-05-15 22:08:03 +0900

    assignment2 (proto)

--- assigment2/Client.c
+++ assigment2/Client.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <pthread.h>
+#include <time.h>
 
 #define BUF_SIZE 100
-#define NAME_SIZE 20
-
-void* send_msg(void* arg);
-void* recv_msg(void* arg);
 void error_handling(char* msg);
 
-char name[NAME_SIZE] = "[DEFAULT]";
+int create_bingo();
+int call_number();
+
+void check_bingo();
+void update_bingo(int number);
+void print_bingo();
+
 char msg[BUF_SIZE];
+int bingo[5][5]; // 빙고판
+int bingo_cnt = 0;
 
 int main(int argc, char* argv[]) {
+    srand(time(NULL));
+
     int sock;
     struct sockaddr_in serv_addr;
-    pthread_t snd_thread, rcv_thread;
     void *thread_return;
 
-    if(argc != 4) {
-        printf("Usage : %s <IP> <PORT> <NAME>\n", argv[0]);
+    int status = 0;
+
+    if(argc != 3) {
+        printf("Usage : %s <IP> <PORT>\n", argv[0]);
         exit(1);
     }
 
-    sprintf(name, "[%s]", argv[3]);
     sock = socket(PF_INET, SOCK_STREAM, 0);
 
     memset(&serv_addr, 0, sizeof(serv_addr));
@@ -37,48 +43,156 @@ int main(int argc, char* argv[]) {
 
     if(connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) error_handling("connect() ERROR");
 
-    pthread_create(&snd_thread, NULL, send_msg, (void*) &sock);
-    pthread_create(&rcv_thread, NULL, recv_msg, (void*) &sock);
-    pthread_join(snd_thread, &thread_return);
-    pthread_join(rcv_thread, &thread_return);
+    // 1. 다른 플레이어 기다리기
+    printf("다른 플레이어를 기다리는 중...\n");
+    read(sock, &status, sizeof(int));
+    if(status != 1) error_handling("read ERROR()");
+    else printf("게임을 시작합니다\n");
+
+    // 2. 빙고판 생성하기
+    status = 0;
+    while(status != 1) status = create_bingo(); // 빙고판 생성
+    write(sock, (char*)&status, sizeof(int));
+    printf("상대방 응답 대기 중...\n");
+
+    // 3. 본격적인 게임 시작
+    read(sock, &status, sizeof(int));
+    printf("\n========== START ==========\n");
+    while(status != -1) {
+        printf("\n현재 내 빙고판\n");
+        print_bingo();
+        printf("빙고 갯수: %d\n", bingo_cnt);
+
+        int number = 0;
+        if(status == 1) {
+            printf("\n당신 차례 입니다\n");
+            number = call_number();
+
+            update_bingo(number);
+
+            write(sock, (char*)&number, sizeof(int));
+            write(sock, (char*)&bingo_cnt, sizeof(int));
+            status = 0;
+        }
+        else if(status == 0) {
+            printf("\n상대방 응답 대기 중...\n");
+            read(sock, &number, sizeof(int));
+            if(number == 0) break; // 상대방이 이겼을 때
+
+            printf("상대방이 부른 숫자: %d\n", number);
+            update_bingo(number);
+
+            status = 1;
+        }
+
+        if(bingo_cnt >= 3) break; // 내가 이겼을 때
+    }
+    printf("==========  END  ==========\n");
+    printf("\n최종 빙고판\n");
+    print_bingo();
+    printf("빙고 갯수: %d\n", bingo_cnt);
+    read(sock, &msg, BUF_SIZE);
+    fputs(msg, stdout);
 
     close(sock);
     return 0;
-}
-
-void* send_msg(void* arg) { // send thread main
-    int sock = *((int*) arg);
-    char name_msg[NAME_SIZE + BUF_SIZE];
-
-    while(1) {
-        fgets(msg, BUF_SIZE, stdin);
-        if(!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")){
-            close(sock);
-            exit(0);
-        }
-        sprintf(name_msg, "%s %s", name, msg);
-        write(sock, name_msg, strlen(name_msg));
-    }
-
-    return NULL;
-}
-void* recv_msg(void* arg) { // read thread main
-    int sock = *((int*) arg);
-    char name_msg[NAME_SIZE + BUF_SIZE];
-    
-    int str_len;
-    while(1) {
-        str_len = read(sock, name_msg, NAME_SIZE + BUF_SIZE - 1);
-        if(str_len == -1) return (void*) -1;
-        name_msg[str_len] = 0;
-        fputs(name_msg, stdout);
-    }
-
-    return NULL;
 }
 
 void error_handling(char* msg) {
     fputs(msg, stderr);
     fputc('\n', stderr);
     exit(1);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+int create_bingo() {
+    printf("\n5X5 빙고판을 작성해주세요\n");
+    printf("1~25까지의 숫자를 띄어쓰기로 구분하여, 한 줄로 25개의 숫자를 입력하면 됩니다\n");
+    printf("ex) 5 21 17 8 6 ... 1 22 13\n");
+    printf("만약 랜덤으로 빙고판을 생성하길 원한다면 0을 입력하세요\n");
+    printf(">> ");
+
+    fgets(msg, BUF_SIZE, stdin);
+    if(!strcmp(msg, "0\n")) {
+        int tmps[25];
+        for(int i=0; i<25; i++) {
+            tmps[i] = (rand() % 25) + 1;
+            for(int j=0; j<i; j++) { if(tmps[j] == tmps[i]) { i--; break; } }
+        }
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<5; j++) { bingo[i][j] = tmps[5*i + j]; }
+        }
+    } else {
+        char* number = strtok(msg, " ");
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<5; j++) {
+                if(number == NULL) {
+                    printf("25개의 숫자를 입력해야 합니다\n");
+                    return -1;
+                }
+                bingo[i][j] = atoi(number);
+                number = strtok(NULL, " ");
+            }
+        }
+    }
+
+    print_bingo();
+    printf("이 빙고판으로 하시겠습니까? (예: 1, 아니오: 2)\n");
+
+    fgets(msg, BUF_SIZE, stdin);
+    if(!strcmp(msg, "1\n")) return 1;
+    else return 0;
+}
+
+int call_number() {
+    printf("숫자를 선택하세요\n");
+    printf(">> ");
+
+    fgets(msg, BUF_SIZE, stdin);
+    return atoi(msg);
+}
+
+void check_bingo() {
+    bingo_cnt = 0;
+    int row[5] = {0, 0, 0, 0, 0};
+    int col[5] = {0, 0, 0, 0, 0};
+    int dia[2] = {0, 0};
+
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<5; j++) {
+            if(bingo[i][j] == 0) {
+                row[i]++;
+                col[j]++;
+                if(i == j) dia[0]++;
+                if(i+j == 4) dia[1]++;
+            }
+        }
+    }
+
+    for(int i=0; i<5; i++) {
+        if(row[i] == 5) bingo_cnt++;
+        if(col[i] == 5) bingo_cnt++;
+    }
+    if(dia[0] == 5) bingo_cnt++;
+    if(dia[1] == 5) bingo_cnt++;
+}
+void update_bingo(int number) {
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<5; j++) {
+            if(bingo[i][j] == number) bingo[i][j] = 0;
+        }
+    }
+    check_bingo();
+}
+void print_bingo() {
+    printf("┏━━━━━━━━━━━━━━━━┓\n");
+    for(int i=0; i<5; i++) {
+        printf("┃");
+        for(int j=0; j<5; j++) {
+            if(bingo[i][j] != 0) printf("%3d", bingo[i][j]);
+            else printf("  X");
+        }
+        printf(" ┃\n");
+    }
+    printf("┗━━━━━━━━━━━━━━━━┛\n");
 }
--- assigment2/Server.c
+++ assigment2/Server.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <pthread.h>
 
 #define BUF_SIZE 100
-#define MAX_CLNT 256
-
-void* handle_clnt(void* arg);
-void send_msg(char* msg, int len);
+int recv_status();
+int recv_number(int clnt);
+void send_status_all(int status);
+void send_status_each_other(int status1, int status2);
+void send_number(int number, int clnt);
 void error_handling(char* msg);
 
-int clnt_cnt = 0;
-int clnt_socks[MAX_CLNT];
-pthread_mutex_t mutx;
+int clnt_socks[2];
+char win_msg[BUF_SIZE] = "당신이 이겼습니다\n";
+char lose_msg[BUF_SIZE] = "당신은 졌습니다\n";
 
 int main(int argc, char* argv[]) {
     int serv_sock, clnt_sock;
     struct sockaddr_in serv_adr, clnt_adr;
-    int clnt_adr_sz;
-    pthread_t t_id;
+    socklen_t clnt_adr_sz; // int clnt_adr_sz;
 
     if(argc != 2) {
         printf("Usage: %s <PORT>\n", argv[0]);
         exit(1);
     }
 
-    pthread_mutex_init(&mutx, NULL);
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
     memset(&serv_adr, 0, sizeof(serv_adr));
@@ -37,52 +34,81 @@ int main(int argc, char* argv[]) {
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_adr.sin_port = htons(atoi(argv[1]));
 
-    if(bind(serv_sock, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) == -1) error_handling("bind() ERROR");
+    if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr)) == -1) error_handling("bind() ERROR");
     if(listen(serv_sock, 5) == -1) error_handling("listen() ERROR");
+    printf("Server open...\n");
 
+    // 1번째 플레이어
+    clnt_adr_sz = sizeof(clnt_adr);
+    clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_adr, &clnt_adr_sz);
+    clnt_socks[0] = clnt_sock;
+    printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+
+    // 2번째 플레이어
+    clnt_adr_sz = sizeof(clnt_adr);
+    clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_adr, &clnt_adr_sz);
+    clnt_socks[1] = clnt_sock;
+    printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+
+    send_status_all(1);
+    printf("게임 시작!\n");
+
+    int status = recv_status(); // 빙고판 생성 대기
+    if(status != 1) error_handling("read ERROR()");
+    send_status_each_other(1, 0); // 빙고 시작 -> 먼저 접속한 플레이어에게 선두권
     while(1) {
-        clnt_adr_sz = sizeof(clnt_adr);
-        clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_adr, &clnt_adr_sz);
+        int number = recv_number(0); // 1번째 플레이어에게 숫자 받음
+        send_number(number, 1); // 2번째 플레이어에게 숫자 전송
+        if(number == 0) { // 빙고! -> 1번째 플레이어 승
+            printf("1번째 플레이어 빙고!\n");
+            write(clnt_socks[0], &win_msg, BUF_SIZE);
+            write(clnt_socks[1], &lose_msg, BUF_SIZE);
+            break;
+        }
 
-        pthread_mutex_lock(&mutx);
-        clnt_socks[clnt_cnt++] = clnt_sock;
-        pthread_mutex_unlock(&mutx);
-
-        pthread_create(&t_id, NULL, handle_clnt, (void *) &clnt_sock);
-        pthread_detach(t_id);
-        printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
+        number = recv_number(1); // 2번째 플레이어에게 숫자 받음
+        send_number(number, 0); // 1번째 플레이어에게 숫자 전송
+        if(number == 0) { // 빙고! -> 2번째 플레이어 승
+            printf("2번째 플레이어 빙고!\n");
+            write(clnt_socks[1], &win_msg, BUF_SIZE);
+            write(clnt_socks[0], &lose_msg, BUF_SIZE);
+            break;
+        }
     }
 
+    close(clnt_socks[0]);
+    close(clnt_socks[1]);
     close(serv_sock);
     return 0;
 }
 
-void* handle_clnt(void* arg) {
-    int clnt_sock = *((int*) arg);
-    int str_len = 0, i;
-    char msg[BUF_SIZE];
-
-    while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0) send_msg(msg, str_len);
-
-    pthread_mutex_lock(&mutx);
-    for(i=0; i<clnt_cnt; i++) { // remove disconnected client
-        if(clnt_sock == clnt_socks[i]) {
-            while(i++ < clnt_cnt-1) clnt_socks[i] = clnt_socks[i+1];
-            break;
-        }
-    }
-    clnt_cnt--;
-    pthread_mutex_unlock(&mutx);
-
-    close(clnt_sock);
-    return NULL;
+int recv_status() {
+    int statuses[2] = {0, 0};
+    for(int i=0; i<2; i++) read(clnt_socks[i], (char*)&statuses[i], sizeof(int));
+    
+    if(statuses[0] == 1 && statuses[1] == 1) return 1;
+    return 0;
 }
-void send_msg(char* msg, int len) { // send to all
-    int i;
+int recv_number(int clnt) {
+    int number = 0; int bingo_cnt = 0;
+    read(clnt_socks[clnt], (char*)&number, sizeof(int));
+    read(clnt_socks[clnt], (char*)&bingo_cnt, sizeof(int));
 
-    pthread_mutex_lock(&mutx);
-    for(i=0; i<clnt_cnt; i++) write(clnt_socks[i], msg, len);
-    pthread_mutex_unlock(&mutx);
+    if(clnt == 0) printf("1번째 플레이어: Call %2d / 현재 빙고 갯수 = %d개\n", number, bingo_cnt);
+    else if(clnt == 1) printf("2번째 플레이어: Call %2d / 현재 빙고 갯수 = %d개\n", number, bingo_cnt);
+
+    if(bingo_cnt >= 3) number = 0;
+    return number;
+}
+void send_status_all(int status) {
+    for(int i=0; i<2; i++) write(clnt_socks[i], (char*)&status, sizeof(int));
+}
+void send_status_each_other(int status1, int status2) {
+    write(clnt_socks[0], (char*)&status1, sizeof(int));
+    write(clnt_socks[1], (char*)&status2, sizeof(int));
+}
+void send_number(int number, int clnt) {
+    write(clnt_socks[clnt], (char*)&number, sizeof(int));
 }
 
 void error_handling(char* msg) {
